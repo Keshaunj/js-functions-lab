23a1a735056ebad48ae9aea47997dbd895a5bdba
Keshaunj <keshaunjones48@gmail.com>
2024-06-27 21:01:09 -0400

    thrusday-class-setup

---
+++ script.js
@@ -0,0 +1,26 @@
+/*
+Exercise 1: maxOfTwoNumbers()
+
+In this exercise, create a function named maxOfTwoNumbers. 
+It should take two numbers as inputs and return the larger number. 
+If they're equal, return either one.
+
+Exercise 1 has been completed for you:
+*/
+
+const maxOfTwoNumbers = (num1,num2) => {
+
+    //num1 - number
+    //num2 - number
+    console.log(num1,num2)  => (num1,num2)
+    // goal - compare the two numbers and return
+    if (x >= y) {
+      return x;
+    } else {
+      return y;
+    }
+  }
+  
+  console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+
+  
