279e4fcf09dd89777b54ab7b5f9f74ed5d6d4837
Keshaunj <keshaunjones48@gmail.com>
2024-07-01 09:24:07 -0400

    updtaed july 2st

--- script.js
+++ script.js
@@ -8,19 +8,52 @@ If they're equal, return either one.
 Exercise 1 has been completed for you:
 */
 
-const maxOfTwoNumbers = (num1,num2) => {
+//const maxOfTwoNumbers = (num5,num6) => {
 
-    num1 = 1
-    num2 = 2
-   console.log(num1,num2)  
+    num5 = 5
+    num6 = 6
+   console.log(num5,num6)  
     // goal - compare the two numbers and return
-    if (num1 >= num2) {
-      return num1
+    if (num5 >= num6) {
+      return num5
     } else {
-      return num2
+      return num6
     }
-  }
   
- // console.log('Exercise 1 Result:', maxOfTwoNumbers(num1, mum2);
+  
+  //console.log('Exercise 1 Result:', maxOfTwoNumbers(num5, mum6)
 
   
+
+  /*
+Exercise 2: isAdult()
+
+Write a function named isAdult. It should take an age (number) 
+and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
+
+Example: isAdult(21) should return 'Adult'.
+
+
+
+
+Complete the exercise in the space below:
+*/
+
+
+//let Adult = 18
+//let Minor = 17
+
+function isAdult(age) {
+  if (age >= 18) {
+    return 'Adult';
+  } else {
+    return 'Minor';
+  }
+}
+//console.log('Exercise 2 Result:', isAdult(21));
+
+
+
+num3 =3
+num4 = 4
+console.log(num3,num4)
