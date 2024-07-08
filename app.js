7da7afec686dc7cb9963743848538ccf9877e34e
Keshaunj <keshaunjones48@gmail.com>
2024-07-08 17:42:49 -0400

    july 8th

--- app.js
+++ app.js
@@ -1,15 +1,1 @@
-/*
-Exercise 6: maxOfThree()
-
-Define a function, maxOfThree. It should accept three numbers 
-and return the largest among them.
-
-Example: maxOfThree(17, 4, 9) should return 17.
-
-Complete the exercise in the space below:
-*/
-
-
-
-console.log('Exercise 6 Result:', maxOfThree(5, 10, 8));
 
